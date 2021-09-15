4027b6d01dc71d6cf455aacbb881cdbeb8f3376f
Yar0Skr <yar.skrypnyk@gmail.com>
2021-09-15 13:41:57 +0300

    feat: updated DTOs. Added validation and mapped types

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,12 @@
+import {IsString} from "class-validator"; //great decorators for validation
+
 export class CreateCoffeeDto {
+    @IsString()
     readonly name: string
+
+    @IsString()
     readonly brand: string
+
+    @IsString({each: true})
     readonly flavors: string[]
 }
--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,6 @@
-export class UpdateCoffeeDto {
-    readonly name?: string
-    readonly brand?: string
-    readonly flavors?: string[]
-}
+import {
+    PartialType, //returns the type of class with all of the properties set to OPTIONAL
+} from "@nestjs/mapped-types";
+import {CreateCoffeeDto} from "./create-coffee.dto";
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto){}
