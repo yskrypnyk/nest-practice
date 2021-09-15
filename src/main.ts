110347f612bbd6963324227a9d2e7b3c8ad8d366
Yar0Skr <yar.skrypnyk@gmail.com>
2021-09-15 14:03:46 +0300

    feat: Added validation and whitelist finished chapter 1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  //use validation pipe
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    //use validation pipe
+    app.useGlobalPipes(
+        new ValidationPipe({
+            //unwanted properties of passed data from client is striped and removed (workflow continues)
+            whitelist: true,
+
+            //non whitelisted properties request object cause errors (corresponding error message)
+            forbidNonWhitelisted: true,
+
+            //converts primitive type into TS-required (@Param) and makes request body an instance of dto but slightly affects performance
+            // transform: true
+        })
+    )
+    await app.listen(3000);
 }
+
 bootstrap();
