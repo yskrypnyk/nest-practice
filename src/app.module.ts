0d471571e1a19179a7d25deea04ad6ef06f23d0c
Yar0Skr <yar.skrypnyk@gmail.com>
2021-09-16 14:42:00 +0300

    feat: env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CoffeesModule } from './coffees/coffees.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {CoffeesModule} from './coffees/coffees.module';
 import {TypeOrmModule} from "@nestjs/typeorm"; //enable database connection
 import {IS_DEV} from './constants/consts'
-import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-import { DatabaseModule } from './database/database.module';
+import {CoffeeRatingModule} from './coffee-rating/coffee-rating.module';
+import {DatabaseModule} from './database/database.module';
+import {ConfigModule} from "@nestjs/config";
+
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type:'postgres',
-    host:'localhost',
-    port:5432,
-    username:'postgres',
-    password:'pass123',
-    database:'postgres',
-    autoLoadEntities: true,
-    //must be disabled in production
-    synchronize: IS_DEV
-  }), CoffeeRatingModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: +process.env.DATABASE_PORT,
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            autoLoadEntities: true,
+            //must be disabled in production
+            synchronize: IS_DEV
+        }),
+        CoffeeRatingModule,
+        DatabaseModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
