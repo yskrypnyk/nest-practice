e9f2222b34fb2a0fddcfcd453450a5ea9fb18b3a
Yar0Skr <yar.skrypnyk@gmail.com>
2021-09-17 16:26:31 +0300

    feat: logging with middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
 import {APP_GUARD} from "@nestjs/core";
 import {ApiKeyGuard} from "./guards/api-key.guard";
 import {ConfigModule} from "@nestjs/config";
+import {LoggingMiddleware} from "./middleware/logging.middleware";
 
 @Module({
     imports: [ConfigModule],
@@ -9,7 +10,15 @@ import {ConfigModule} from "@nestjs/config";
         {
             provide: APP_GUARD,
             useClass: ApiKeyGuard
-        }
+        },
+
     ]
 })
-export class CommonModule {}
+export class CommonModule implements NestModule{
+    //connecting middleware
+    configure(consumer: MiddlewareConsumer){
+        // consumer.apply(LoggingMiddleware).forRoutes({path: "coffees", method: RequestMethod.GET})
+        // consumer.apply(LoggingMiddleware).exclude("coffees").forRoutes("*")
+        consumer.apply(LoggingMiddleware).forRoutes("*")
+    }
+}
