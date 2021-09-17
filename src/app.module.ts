4f4d9afe31973bff5cf19b3ce7d3a0712a296564
Yar0Skr <yar.skrypnyk@gmail.com>
2021-09-17 11:41:46 +0300

    feat: env validation using @hapi/joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {CoffeesModule} from './coffees/coffees.module';
+import {CoffeesModule} from './coffees/coffees.module'; //main coffee module
 import {TypeOrmModule} from "@nestjs/typeorm"; //enable database connection
 import {IS_DEV} from './constants/consts'
-import {CoffeeRatingModule} from './coffee-rating/coffee-rating.module';
-import {DatabaseModule} from './database/database.module';
-import {ConfigModule} from "@nestjs/config";
+import {CoffeeRatingModule} from './coffee-rating/coffee-rating.module'; //secondary coffee module
+import {DatabaseModule} from './database/database.module'; //module that can be used to connect to database
+import {ConfigModule} from "@nestjs/config"; //add env file as variable
+import * as Joi from "@hapi/joi"; //validation for env file variables
 
 @Module({
     imports: [
         /** local configuration module */
         ConfigModule.forRoot({
             ignoreEnvFile: !IS_DEV,
-            envFilePath: '.env'
+            envFilePath: '.env',
+            validationSchema: Joi.object({
+                DATABASE_HOST: Joi.required(),
+                DATABASE_PORT: Joi.number().default(5432)
+            }) //validates env files
         }),
 
         /** Database connection module */
