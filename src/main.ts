9db6580f59273250c68f122d9b35f6de724525f4
Yar0Skr <yar.skrypnyk@gmail.com>
2021-09-17 15:17:15 +0300

    feat: guards and api key

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {HttpExceptionFilter} from "./common/filter/http-exception.filter";
+import {ApiKeyGuard} from "./common/guards/api-key.guard";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -26,6 +27,9 @@ async function bootstrap() {
 
     //use HttpExceptionFilter filter
     app.useGlobalFilters(new HttpExceptionFilter())
+
+    //use guard
+    app.useGlobalGuards(new ApiKeyGuard())
 
     await app.listen(3000);
 }
